25fc21a72cbe5e8b74b59b7c0bb05575eab491f0
Soca <socadan@gmail.com>
2025-08-20 17:41:53 -0300

    Limpia y reduce el código

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-//El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-console.log("Bienvenido");
-//variables:
 let listaDeAmigos = [];
-//-------------------------------------------
-//funciones:
-//función para el botón añadir - añade un amigo a la listaDeAmigos
+
+//función para el botón añadir
 function agregarAmigo() {
-    //para capturar el contenido del input
     let amigoDelUsuario = document.getElementById("amigo").value;
     
-    //verifica si está vacía la variable amigoDelUsuario o no
     if (amigoDelUsuario === "") {
         alert("Por favor, ingrese un nombre");
     
@@ -22,21 +16,15 @@ function agregarAmigo() {
     }
     console.log(listaDeAmigos);
     console.log(listaDeAmigos.length);
-    //limpia el input
+
         document.getElementById("amigo").value = "";
-    //muestra la lista de los amigos ingresados
         mostrarListaDeAmigos();
 }
 
-//funcion para mostrar en pantalla a los amigos que agregas a tu lista
+//esta función muestra los amigos que se han ido subiendo
 function mostrarListaDeAmigos() {
-    //conecta la lista de HTML con JS por medio del ID de HTML
     let listaDeAmigosHTML = document.getElementById("listaAmigos");
-
-    //limpia la lista para no mostrar duplicados
     listaDeAmigosHTML.innerHTML = "";
-
-    //recorre la lista y crea un nuevo elemento de la lista
     listaDeAmigos.forEach(amigo => {
         let nuevoElemento = document.createElement("li");
         nuevoElemento.textContent = amigo;
@@ -44,16 +32,13 @@ function mostrarListaDeAmigos() {
     });
 } 
 
-//función para elegir a un amigo de la lista de amigos de manera aleatoria
+//esta función realiza el sorteo
 function sortearAmigo() {
     let numeroDeAmigos = listaDeAmigos.length;
     let numeroAleatorio = 0;
     let mostrarResultado;
 
     if(numeroDeAmigos < 3 || numeroDeAmigos > 10) {
-        //let mensaje = document.querySelector('h2');
-        //mensaje.innerHTML = 'Por favor, ingrese un mínimo de 3 nombres y un máximo de 10'; 
-
         alert("Por favor, inserte un mínimo de 3 nombres y un máximo de 10");
     } else {
             numeroAleatorio = Math.floor(Math.random()*numeroDeAmigos);
